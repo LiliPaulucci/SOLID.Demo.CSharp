70f0d67b3a460990081a997612bc4afe22680183
alinepaulucci <alinepaulucci@DESKTOP-APL27CG>
2017-09-07 17:26:45 -0300

    Add project files.

---
+++ SOLID.Demo.Converters/ArabicToCardinalConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Demo.Converters
+{
+    public class ArabicToCardinalConverter
+    {
+        public String Convert (int IncomeNumber)
+        {
+            if (IncomeNumber < 0 || IncomeNumber > 99)
+                throw new NotSupportedException("Numbers should be Integers between 0 and 99.");
+
+            string[] CardinalUnits = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eightteen", "Nineteen" };
+            string[] CardinalTens = new string[] { "Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+            String OutcomeNumber;
+
+            if (IsATen(IncomeNumber))
+                OutcomeNumber = CardinalUnits[IncomeNumber];
+            else if (IncomeNumber < 20)
+                OutcomeNumber = CardinalUnits[IncomeNumber];
+            else
+            {
+                Int32 Ten = GetQuotientOfDivisionBy10(IncomeNumber);
+                Int32 Remainder = GetRemainderOfDivisionBy10(IncomeNumber);
+
+                OutcomeNumber = CardinalTens[Ten] + CardinalUnits[Remainder];
+            }
+
+            return OutcomeNumber;
+        }
+
+        private bool IsATen(int IncomeNumber)
+        {
+            return IncomeNumber % 10 == 0;
+        }
+
+        private Int32 GetRemainderOfDivisionBy10(int IncomeNumber)
+        {
+            return IncomeNumber % 10;
+        }
+
+        private Int32 GetQuotientOfDivisionBy10(int IncomeNumber)
+        {
+            return IncomeNumber / 10;
+        }
+    }
+}
---
+++ SOLID.Demo.Tests/ArabicToCardinalConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOLID.Demo.Converters;
+
+namespace SOLID.Demo.Tests
+{
+    [TestClass]
+    public class ArabicToCardinalConverterTests
+    {
+        ArabicToCardinalConverter arabicToCadinalConverter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            arabicToCadinalConverter = new ArabicToCardinalConverter();
+        }
+
+        [TestMethod]
+        public void ShouldNotBeNullWhenCreateInstance()
+        {
+            Assert.IsNotNull(arabicToCadinalConverter);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStringWhenReceiveInteger()
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(arabicToCadinalConverter.Convert(4)));
+        }
+
+        [TestMethod]
+        public void ShouldReturnZeroWhenReceive0()
+        {
+            Assert.AreEqual("Zero", arabicToCadinalConverter.Convert(0));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFourWhenReceive4()
+        {
+            Assert.AreEqual("Four", arabicToCadinalConverter.Convert(4));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTenWhenReceive10()
+        {
+            Assert.AreEqual("Ten", arabicToCadinalConverter.Convert(10));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFourteenWhenReceive14()
+        {
+            Assert.AreEqual("Fourteen", arabicToCadinalConverter.Convert(14));
+        }
+
+        [TestMethod]
+        public void ShouldReturnThirtyFiveWhenReceive35()
+        {
+            Assert.AreEqual("ThirtyFive", arabicToCadinalConverter.Convert(35));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFortyWhenReceive40()
+        {
+            Assert.AreEqual("ThirtyFive", arabicToCadinalConverter.Convert(35));
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException), "Numbers should be Integers between 0 and 99.")]
+        public void ShouldReturnExceptionWhenNumberIsLessThan0()
+        {
+            arabicToCadinalConverter.Convert(-2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException), "Numbers should be Integers between 0 and 99.")]
+        public void ShouldReturnExceptionWhenNumberIs100()
+        {
+            arabicToCadinalConverter.Convert(100);
+        }
+    }
+}
