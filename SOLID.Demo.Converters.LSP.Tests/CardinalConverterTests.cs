8bd1ca4daf30d619e148e56c50af6f57027a4ecd
alinepaulucci <alinepaulucci@gmail.com>
2017-09-07 21:12:17 -0300

    Implementing Liskov Substitution Principle

--- SOLID.Demo.Converters.LSP.Tests/CardinalConverterTests.cs
+++ SOLID.Demo.Converters.LSP.Tests/CardinalConverterTests.cs
@@ -1,77 +1,80 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SOLID.Demo.Converters.OCP.Tests
+namespace SOLID.Demo.Converters.LSP.Tests
 {
     [TestClass]
     public class CardinalConverterTests
     {
-        ArabicToCardinalConverter arabicToCadinalConverter;
+        CardinalConverter arabicToCadinalConverter;
+        /*
+         * The differente between this code and OCP test is that I`m declaring a generic type and using a especific implementation
+         * */
 
         [TestInitialize]
-        public void OCP_Initialize()
+        public void LSP_Initialize()
         {
             arabicToCadinalConverter = new ArabicToCardinalConverter();
         }
 
         [TestMethod]
-        public void OCP_ShouldNotBeNullWhenCreateInstance()
+        public void LSP_ShouldNotBeNullWhenCreateInstance()
         {
             Assert.IsNotNull(arabicToCadinalConverter);
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnStringWhenReceiveInteger()
+        public void LSP_ShouldReturnStringWhenReceiveInteger()
         {
             Assert.IsFalse(string.IsNullOrEmpty(arabicToCadinalConverter.Convert(4)));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnZeroWhenReceive0()
+        public void LSP_ShouldReturnZeroWhenReceive0()
         {
             Assert.AreEqual("Zero", arabicToCadinalConverter.Convert(0));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnFourWhenReceive4()
+        public void LSP_ShouldReturnFourWhenReceive4()
         {
             Assert.AreEqual("Four", arabicToCadinalConverter.Convert(4));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnTenWhenReceive10()
+        public void LSP_ShouldReturnTenWhenReceive10()
         {
             Assert.AreEqual("Ten", arabicToCadinalConverter.Convert(10));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnFourteenWhenReceive14()
+        public void LSP_ShouldReturnFourteenWhenReceive14()
         {
             Assert.AreEqual("Fourteen", arabicToCadinalConverter.Convert(14));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnThirtyFiveWhenReceive35()
+        public void LSP_ShouldReturnThirtyFiveWhenReceive35()
         {
             Assert.AreEqual("Thirty Five", arabicToCadinalConverter.Convert(35));
         }
 
         [TestMethod]
-        public void OCP_ShouldReturnFortyWhenReceive40()
+        public void LSP_ShouldReturnFortyWhenReceive40()
         {
             Assert.AreEqual("Fourty", arabicToCadinalConverter.Convert(40));
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException), "Numbers should be Integers between 0 and 99.")]
-        public void OCP_ShouldReturnExceptionWhenNumberIsLessThan0()
+        public void LSP_ShouldReturnExceptionWhenNumberIsLessThan0()
         {
             arabicToCadinalConverter.Convert(-2);
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException), "Numbers should be Integers between 0 and 99.")]
-        public void OCP_ShouldReturnExceptionWhenNumberIs100()
+        public void LSP_ShouldReturnExceptionWhenNumberIs100()
         {
             arabicToCadinalConverter.Convert(100);
         }
--- SOLID.Demo.Converters.LSP/ArabicToCardinalConverter.cs
+++ SOLID.Demo.Converters.LSP/ArabicToCardinalConverter.cs
@@ -13,7 +13,28 @@ namespace SOLID.Demo.Converters.LSP
      * Let q(x) be a property provable about objects of x of type T. Then q(y) should be provable for objects y of type S where S is a subtype of T.
      * */
 
-    public class ArabicToCardinalConverter
+    public class ArabicToCardinalConverter : CardinalConverter
     {
+        public override String Convert(int number)
+        {
+            if (number < 0 || number > 99)
+                throw new NotSupportedException("Numbers should be Integers between 0 and 99.");
+
+            String OutcomeNumber;
+
+            if (MathUtils.IsATen(number))
+                OutcomeNumber = GetCardinalTens(MathUtils.GetQuotientOfDivisionBy10(number));
+            else if (number < 20)
+                OutcomeNumber = GetCardinalUnits(number);
+            else
+            {
+                Int32 Ten = MathUtils.GetQuotientOfDivisionBy10(number);
+                Int32 Remainder = MathUtils.GetRemainderOfDivisionBy10(number);
+
+                OutcomeNumber = GetCardinalTens(Ten) + " " + GetCardinalUnits(Remainder);
+            }
+
+            return OutcomeNumber;
+        }
     }
 }
